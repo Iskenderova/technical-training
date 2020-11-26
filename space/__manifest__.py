1a6d6506c7ed7ab6ba6969371246e31e3e398b05
Irada Iskenderova <iskenderova@rising-systems.de>
2020-11-26 18:24:52 +0100

    added mode., error in odoo import

---
+++ space/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    
+    'name': 'Space Mission',
+    
+    'summary': """Space Mission""",
+    
+    'description': """
+        Get to the moon:
+        -Rocket
+    
+    """,
+    
+    'author': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Mission',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    
+    'demo': [
+        
+    ],    
+}
